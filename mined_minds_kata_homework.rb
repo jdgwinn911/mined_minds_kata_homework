43c13c0f792492a76bbd71787d6a3178e71480c1
Josh Gwinn <jdgwinn911@gmail.com>
2018-06-25 00:24:23 -0400

    second fix

--- mined_minds_kata_homework.rb
+++ mined_minds_kata_homework.rb
@@ -1,6 +1,21 @@
 def katarray()
     katarr = []
     val = 0
-    val += 1
-    katarr << val 
+    loop do
+        val += 1
+        katarr << val
+        if val == 100
+            break
+        end
+    end
+    if katarr[2] % 3 == 0
+        val = "mined"
+        katarr.insert(2, val)
+        katarr.delete(3)
+    end
+    katarr
+
+
+
+
 end
